2b9f83087c20e759ffcf46f473d80e8a09e7e663
Jill.Ward <jill.ward@FT-MW5134.local>
2019-07-31 13:29:23 +0100

    Updated lambda for approvals bot

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-const AWS = require('aws-sdk');
-const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
-
+const { sendSlackMessage } = require('./sendToSlack');
 exports.handler = async (event) => {
-	// TODO implement
-
-    const eventBody = JSON.parse(event.Records[0].body);
+	const eventBody = JSON.parse(event.Records[0].body);
     const content = JSON.parse(eventBody.Message).content;
     const objectString = content.match(/\{([\s\S]*?)\}/g);
     const finalObject = JSON.parse(objectString[0].replace(/(\r\n|\n|\r)/gm, ""));
-    
-    console.log('processing sqs')
-    console.log(finalObject)
-    console.log(`email is ${finalObject.emailAddress}`)
-    console.log(`cost is ${finalObject.cost}`)
+
+
+    // console.log('processing sqs')
+    // console.log(finalObject)
+    // console.log(`email is ${finalObject.emailAddress}`)
+    // console.log(`cost is ${finalObject.cost}`)
+
+    sendSlackMessage(finalObject);
 };
--- src/sendToSlack.js
+++ src/sendToSlack.js
@@ -1,17 +1,26 @@
 const Slack = require('slack-node');
 
 module.exports = {
-	sendSlackMessage: function sendSlackMessage(content) {
-		const webhookUri = 'https://hooks.slack.com/services/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'
-		slack = new Slack();
-		slack.setWebhook(webhookUri);
-		slack.webhook({
-			channel: "#junior-squad-2",
-			username: "ip-approvals-test",
-			icon_emoji: "https://cdn.glitch.com/899ccd26-b39f-4d00-a935-fd2940151804%2FIMG_6418.JPG?v=1563378319559",
-			text: `${content.emailAddress} wants £${content.cost} for ${content.reason} :shocked-pikachu:`
-		}, function (err, response) {
-			console.log(response);
-		});
-	}
+    sendSlackMessage: (details) => {
+        const { emailAddress, cost, reason } = details
+        console.log(`${emailAddress} wants £${cost} for ${reason}`)
+
+        const webhookUri = "https://hooks.slack.com/services/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";
+        slack = new Slack();
+        slack.setWebhook(webhookUri);
+        
+        slack.webhook({
+        channel: "#junior-squad-2",
+        username: "Approvals Bot",
+        icon_emoji: "https://www.pngix.com/pngfile/big/0-7360_hand-holding-cash-money-hand-holding-money-png.png",
+        text: `${emailAddress} wants £${cost} for ${reason}`
+        }, function(err, response) {
+            if(err) {
+                console.log('An error has occured', err);
+            } else  {
+            console.log('message has been sent to Slack', response)
+            }
+        });
+
+    }
 }
