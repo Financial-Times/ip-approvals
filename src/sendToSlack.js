d15289811e53d06957513c8c07568c3bb46765b2
Jill.Ward <jill.ward@ft-mw5134.osb.ft.com>
2019-07-31 15:37:39 +0100

    Updated form data and send to Slack javascript

--- src/sendToSlack.js
+++ src/sendToSlack.js
@@ -2,7 +2,7 @@ const Slack = require('slack-node');
 
 module.exports = {
     sendSlackMessage: (details) => {
-        const { emailAddress, cost, reason } = details
+        const { emailAddress, cost, reason, url, calendarYear, travelCost, additionalInfo } = details
         console.log(`${emailAddress} wants £${cost} for ${reason}`)
 
         const webhookUri = "https://hooks.slack.com/services/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";
@@ -13,7 +13,31 @@ module.exports = {
         channel: "#junior-squad-2",
         username: "Approvals Bot",
         icon_emoji: "https://www.pngix.com/pngfile/big/0-7360_hand-holding-cash-money-hand-holding-money-png.png",
-        text: `${emailAddress} wants £${cost} for ${reason}`
+        attachments: [
+            {
+              "fallback": "Approve button",
+              "actions": [
+                {
+                  "type": "button",
+                  "text": "Approve",
+                  "style": "primary",
+                  "url": "https://flights.example.com/book/r123456"
+                },
+                {
+                    "type": "button",
+                    "text": "Deny",
+                    "style": "danger",
+                    "url": "https://flights.example.com/book/r123456"
+                  }
+              ]
+            }
+          ],
+        text: `Hi! ${emailAddress} has sent through a new ${reason} request. 
+        • URL: ${url}. 
+        • Cost: £${cost}. 
+        • Travel/accomodation cost: £${travelCost}. 
+        • Calendar year: ${calendarYear}. 
+        • Additional info: ${additionalInfo}.`
         }, function(err, response) {
             if(err) {
                 console.log('An error has occured', err);
