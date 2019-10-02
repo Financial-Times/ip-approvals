63214809742d7c8a3896bcf36046a0e3980cbc75
Kate Beard <asouthernbellein@gmail.com>
2019-10-02 15:02:12 +0100

    Refactor using promises

--- src/sendToSlack.js
+++ src/sendToSlack.js
@@ -1,5 +1,7 @@
-const Slack = require('slack-node');
+//const Slack = require('slack-node');
 const fetch = require('node-fetch');
+const moment = require('moment');
+
 require('dotenv').config()
 
 async function peopleApiCall(person) {
@@ -15,77 +17,102 @@ async function peopleApiCall(person) {
   return response.json()
 }
 
-module.exports = {
-  sendSlackMessage: async (details) => {
+async function slackMessage(url, options) {
+  const response = await fetch(url, options)
 
-    const { emailAddress, cost, reason, url, calendarYear, travelCost, additionalInfo } = details
+  const body = response.json()
 
-    console.log(`${emailAddress} wants £${cost} for ${reason}`)
+  console.log(body)
 
-    const person = emailAddress.split('@')[0]
-
-    const response = await peopleApiCall(person)
-
-    console.log("approver", response[0].finance[0].name, "slack id", response[0].slack.id)
-
-    const secondResponse = await peopleApiCall(response[0].finance[0].name)
-
-    console.log("approver's slack id", secondResponse[0].slack.id)
-
-    // hard-coded to Kate's Slack id but will use info above
-    const approver = response[0].slack.id
-
-    // also hard-coded to Kate's id
-    const requester = response[0].slack.id
-
-    const webhookUri = "https://hooks.slack.com/services/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";
-    slack = new Slack();
-    slack.setWebhook(webhookUri);
-
-    slack.webhook({
-      channel: `${approver}`,
-      username: "Approvals Bot",
-      icon_emoji: "https://www.pngix.com/pngfile/big/0-7360_hand-holding-cash-money-hand-holding-money-png.png",
-      attachments: [
-        {
-          "fallback": "Approve button",
-          "actions": [
-            {
-              "type": "button",
-              "text": "Approve",
-              "style": "primary",
-              "url": "https://lursqeu722.execute-api.eu-west-1.amazonaws.com/prod"
-            },
-            {
-              "type": "button",
-              "text": "Deny",
-              "style": "danger",
-              "url": "https://flights.example.com/book/r123456"
-            }
-          ]
-        }
-      ],
-      text: `Hi! <@${requester}> has sent through a new ${reason} request. 
-        • URL: ${url}. 
-        • Cost: £${cost}. 
-        • Travel/accomodation cost: £${travelCost}. 
-        • Calendar year: ${calendarYear}. 
-        • Additional info: ${additionalInfo}.`
-    }, function (err, response) {
-      if (err) {
-        console.log('An error has occured', err);
-      } else {
-        console.log('message has been sent to Slack', response.response)
-      }
-    });
-
-  }
+  return body
 }
 
-/* to do :
+module.exports = {
+  sendSlackMessage: () => {
+    // console.log(details);
+    // const { emailAddress, cost, reason, url, calendarYear, travelCost, additionalInfo } = details
 
-- selectn for fields
-- fix url fields so they don't render weirdly
-- if no addtional info, then don't put 'undefined'
+    // console.log(`${emailAddress} wants £${cost} for ${reason}`)
 
-*/
+    // const person = emailAddress.split('@')[0]
+
+    // console.log('Calling People API')
+    // const response = await peopleApiCall(person)
+
+    // console.log("approver", response[0].finance[0].name, "slack id", response[0].slack.id)
+
+    // const secondResponse = await peopleApiCall(response[0].finance[0].name)
+
+    // console.log("approver's slack id", secondResponse[0].slack.id)
+
+    // // hard-coded to Kate's Slack id but will use info above
+    // const approver = response[0].slack.id
+
+    // // also hard-coded to Kate's id
+    // const requester = response[0].slack.id
+    // // const requester = emailAddress;
+
+    // const message = {
+    //   "username": "Approvals Bot",
+    //   "icon_emoji": "https://www.pngix.com/pngfile/big/0-7360_hand-holding-cash-money-hand-holding-money-png.png",
+    //   "attachments": [
+    //     {
+    //       "fallback": "Approve button",
+    //       "attachment_type": "default",
+    //       "attachments": [{
+    //         "text": "Approve"
+    //       }],
+    //       "callback_id": "123",
+    //       "actions": [
+    //         {
+    //           "name": "approve",
+    //           "type": "button",
+    //           "text": "Approve",
+    //           "style": "primary",
+    //           "value": "yeeeee"
+    //         },
+    //         {
+    //           "name": "deny",
+    //           "type": "button",
+    //           "text": "Deny",
+    //           "style": "danger",
+    //           "value": "hawwwwwwww"
+    //         }
+    //       ]
+    //     }
+    //   ],
+    //   "text": "boooooo",
+		// "channel": "U03E98JJN"
+    // }
+
+    return new Promise((resolve, reject) => {
+      console.log('trying to send');
+
+      const message = {
+        "username":"Mopsa", 
+        "text":":corn: Hi Jill",
+        "channel":"U03E98JJN",
+        "icon_emoji": ":corn:"
+    }
+      const recipient = 'U03E98JJN'
+      const url = 'https://hooks.slack.com/services/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'
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(message)
+      }
+
+      fetch(url, options)
+      .then(response => {
+        console.log('response', response.statusText)
+        return resolve(response)
+      })
+      .catch(err => {
+        console.log(err)
+        return reject(err)
+      })
+    })
+  }
+}
