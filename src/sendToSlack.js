56f42cd7c5e3efecdcab0453c80661793bb6e498
Jill.Ward <jill.ward@ft-mw5134.osb.ft.com>
2019-11-27 15:31:25 +0000

    Message sent back to requester

--- src/sendToSlack.js
+++ src/sendToSlack.js
@@ -1,14 +1,10 @@
 const fetch = require('node-fetch');
-//const moment = require('moment');
-
 require('dotenv').config()
 
+//once we have the approver name from the first api call (below) make another call to get their slack id
 const secondPeopleAPIcall = (approver) => {
-
   approver = approver.replace(' ', '.')
-
   const peopleAPIurl = `https://ip-people.herokuapp.com/api/people/${approver}`
-
   const options = {
     method: 'GET',
     headers: {
@@ -19,11 +15,11 @@ const secondPeopleAPIcall = (approver) => {
   return new Promise((resolve, reject) => {
     fetch(peopleAPIurl, options)
       .then(response => {
-        console.log('response number 2', response.statusText)
+        console.log('peopleAPI response 2: ', response.statusText)
         return response.json();
       })
       .then(json => {
-        console.log('approver slack id', json[0].slack.id)
+        console.log('approver slack id:' , json[0].slack.id)
         resolve({
           approverId: json[0].slack.id,
         })
@@ -31,6 +27,9 @@ const secondPeopleAPIcall = (approver) => {
   })
 }
 
+//the first people api call defined here and run below gets the approver name from the requester's details.
+//it calls in the secondPeopleAPI function defined above which gets the get the approvers slack id.
+//at the end we have the four things we need: approverId, approverName, requesterId and requesterName
 const peopleApiCall = (person) => {
   const peopleAPIurl = `https://ip-people.herokuapp.com/api/people/${person}`
 
@@ -44,11 +43,11 @@ const peopleApiCall = (person) => {
   return new Promise((resolve, reject) => {
     fetch(peopleAPIurl, options)
       .then(response => {
-        console.log('response', response.statusText)
+        console.log('peopleAPI response 1: ', response.statusText)
         return response.json();
       })
       .then(json => {
-        console.log('approver name, ', json[0].finance[0].name, ' requester id, ', json[0].slack.id)
+        console.log('approver name is: ', json[0].finance[0].name, ' requester id is: ', json[0].slack.id)
         secondPeopleAPIcall(json[0].finance[0].name)
           .then(result => {
             resolve({
@@ -66,30 +65,29 @@ const peopleApiCall = (person) => {
   })
 }
 
+//send slack messages to the requester and approvers containing details of the request
 module.exports = {
   sendSlackMessage: (details) => {
-    console.log(details);
+    console.log('details of the request: ', details);
     const { emailAddress, cost, reason, url, calendarYear, travelCost, additionalInfo, uuid } = details
-
-    console.log(`${emailAddress} wants £${cost} for ${reason}`)
-
+    //person is the requester's email address before the @ symbol
     const person = emailAddress.split('@')[0]
 
     return new Promise((resolve, reject) => {
       console.log('trying to send');
+      console.log('people api call');
 
-      console.log('people api call')
-
+      //make the peopleApi call, get the variables we need, then send the slack messages.
       peopleApiCall(person)
         .then(result => {
 
           console.log(result)
 
           const messageForRequester = {
-            // text that appears in Slack notification
+            //text that appears in slack notification.
             text: `Hi ${result.requesterName}, your approver ${result.approverName} has received your new ${reason} request.\n• Cost: £${cost}\n• Travel/accomodation cost: £${travelCost}\n• Calendar year: ${calendarYear}\n• Booking url: ${url}\n• Additional info: ${additionalInfo}\n• Request id: ${uuid}`,
             channel: `${result.requesterId}`,
-            // text that appears in Slack message
+            //text that appears in slack message.
             blocks: [
               {
                 type: 'section',
@@ -102,11 +100,15 @@ module.exports = {
           }
 
           const messageForApprover = {
-            // if testing locally, change channel to a user's Slack id else result.approverId to send to real budget approver.
+            //attention: if testing locally, change this user's slack id else result.approverId is sent to real budget approver.
+            //to send to actual budget approver, use channel: result.approverId
+            //to send to Jill, for example, use channel: `U03E98JJN`,
             channel: `U03E98JJN`,
-            // text that appears in the Slack notification
+            //channel: result.approverId,
+            //text that appears in the slack notification
             text: `Hi ${result.approverName}, you have a new ${reason} request from ${result.requesterName}\n• Cost: £${cost}\n• Cost: £${cost}\n• Travel/accomodation cost: £${travelCost}\n• Calendar year: ${calendarYear}\n\n• Url: ${url}• Additional info: ${additionalInfo}\n• Request id: ${uuid}`,
-            // text that appears in the Slack id
+            // text that appears in the slack id
+            
             blocks: [
               {
                 "type": "section",
@@ -141,7 +143,7 @@ module.exports = {
               }
             ]
           }
-
+          //provide token and connect to slack to send messages
           const slackUrl = "https://slack.com/api/chat.postMessage"
 
           fetch(slackUrl, {
@@ -154,7 +156,7 @@ module.exports = {
           })
           .then(response => response.json())
           .then(data =>  {
-              console.log('response for approver', data)
+              console.log('response for approver: ', data);
               return resolve(data)
             })
             .catch(err => {
